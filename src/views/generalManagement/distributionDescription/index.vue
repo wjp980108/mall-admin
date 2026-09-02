10089da97dbd06db599c7d45a24a8f69b860b304
wjp <wjp_github@163.com>
2026-09-02 19:45:27 +0800

    feat(pages): [分销说明] 新增页面

---
+++ src/views/generalManagement/distributionDescription/index.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+defineOptions({ name: 'GeneralManagementDistributionDescription' });
+
+const content = ref('');
+
+function handleSave() {
+  ElMessage.info('分销说明保存接口暂未接入');
+}
+</script>
+
+<template>
+  <div class="main-container">
+    <app-card class="min-h-0 flex flex-1 flex-col">
+      <app-editor v-model="content" class="distribution-description-editor" />
+      <div class="flex justify-end pt-12">
+        <el-button type="primary" @click="handleSave">
+          保存
+        </el-button>
+      </div>
+    </app-card>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.distribution-description-editor {
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  min-height: 0;
+
+  :deep(.app-editor-content) {
+    flex: 1;
+    min-height: 0;
+    height: auto !important;
+  }
+
+  :deep(.w-e-scroll) {
+    overflow-y: auto;
+  }
+}
+</style>
